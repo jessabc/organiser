fc0e7326f532cae2c64145ea6a5c64d5f67c4063
jess <120062852+jessabc@users.noreply.github.com>
2024-03-09 19:21:03 +1100

    table for medium and up screens

--- src/app/(pages)/finance/page.tsx
+++ src/app/(pages)/finance/page.tsx
@@ -1,13 +1,28 @@
 "use client"
 
 import TransactionSection from "@/app/_components/finance/recent-transactions-overview/TransactionSection"
+import TransactionRow from "@/app/_components/finance/table/TransactionRow"
+import TransactionTable from "@/app/_components/finance/table/TransactionTable"
+import { useAppSelector } from "@/app/_redux/hooks"
+import { RootState } from "@/app/_redux/store"
+import { Card } from "@/app/types/interfaces"
 import React from "react"
 
 export default function FinancePage() {
+
+  const card: Card = useAppSelector((state: RootState) => state.card.value)
+
+  const transactionRowEl = card?.transactions?.map(transaction => <TransactionRow key={transaction.id} transaction={transaction} />)
+
   return (
     <div>
       <title>Organiser | Finance</title> 
-      <TransactionSection/>
+      <div className="md:hidden">
+        <TransactionSection/>
+      </div>
+      <div className="hidden md:block">
+        <TransactionTable transactionRowEl={transactionRowEl}/>
+      </div>
     </div>
   )
 }
