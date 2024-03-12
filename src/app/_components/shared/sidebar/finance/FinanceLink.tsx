a8d4b9aea63edf88380c7713c7ec6d81d856abf7
jess <120062852+jessabc@users.noreply.github.com>
2024-03-12 14:38:26 +1100

    fix dark hover

--- src/app/_components/shared/sidebar/finance/FinanceLink.tsx
+++ src/app/_components/shared/sidebar/finance/FinanceLink.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+ 
 import React, { useState } from "react"
 import Link from "next/link"
 
@@ -13,7 +13,7 @@ export default function FinanceLink({link}: Props) {
     <li>
       <Link 
         href={`/finance/${link.toLowerCase()}`} 
-        className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-slate-700 rounded-lg hover:bg-gray-100 dark:bg-gray-800 dark:text-slate-400 dark:hover:text-slate-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+        className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:focus:bg-gray-700"
         >
         {link}
     </Link>
