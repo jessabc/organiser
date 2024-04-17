4c5c03de0d2e538cfb81e8e5a3529cbc18f746d6
jess <120062852+jessabc@users.noreply.github.com>
2024-04-17 12:10:35 +1000

    remove getAdvice

--- src/app/_components/shared/header/Header.tsx
+++ src/app/_components/shared/header/Header.tsx
@@ -1,72 +1,124 @@
-import React, { useEffect, useState } from 'react'
-import Link from 'next/link'
-import getAdvice from '@/app/_helpers/getAdvice'
-import ThemeSwitch from '../../ThemeSwitch'
-import getGreeting from '@/app/_helpers/getGreeting'
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import ThemeSwitch from "../../ThemeSwitch";
+import getGreeting from "@/app/_helpers/getGreeting";
 
 export default function Header() {
+  const greeting = getGreeting();
 
-  // const [advice, setAdvice] = useState('')
-
-  const greeting = getGreeting()
-
-  // useEffect(() => {
-  //   async function getSetAdvice() {
-  //     const advice = await getAdvice()
-  //     setAdvice(advice) 
-  //   }
-  //   getSetAdvice() 
-  // }, [])
-  
   return (
     <header className="sticky top-0 inset-x-0 flex flex-wrap sm:justify-start sm:flex-nowrap  w-full bg-white border-b text-sm py-2.5 sm:py-4 lg:ps-64 dark:bg-gray-800 dark:border-gray-700 z-[5]">
       {/* removed z-[48] so modal iwll work */}
-      <nav className="flex basis-full items-center w-full mx-auto px-4 sm:px-6 md:px-8" aria-label="Global">
+      <nav
+        className="flex basis-full items-center w-full mx-auto px-4 sm:px-6 md:px-8"
+        aria-label="Global"
+      >
         <div className="me-5 lg:me-0 lg:hidden">
-          <Link className="flex-none text-2xl  dark:text-white font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-indigo-300 to-indigo-800" href={"/"} aria-label="Brand">Organiser.</Link>
+          <Link
+            className="flex-none text-2xl  dark:text-white font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-indigo-300 to-indigo-800"
+            href={"/"}
+            aria-label="Brand"
+          >
+            Organiser.
+          </Link>
         </div>
 
         <div className="w-full flex items-center justify-end ms-auto lg:justify-between sm:gap-x-3 sm:order-3">
+          <h2 className="text-xl text-gray-700 dark:text-white font-bold hidden lg:flex">{`${greeting}, Guest!`}</h2>
 
-          <h2 className='text-xl text-gray-700 dark:text-white font-bold hidden lg:flex'>{`${greeting}, Guest!`}</h2>
-
-          {/* advice api */}
-          {/* {advice ? 
-          (<div className=' hidden lg:flex'>
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-quote rotate-180" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 11h-4a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h3a1 1 0 0 1 1 1v6c0 2.667 -1.333 4.333 -4 5" /><path d="M19 11h-4a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h3a1 1 0 0 1 1 1v6c0 2.667 -1.333 4.333 -4 5" /></svg>
-              <h2 className='text-xl text-gray-700 dark:text-white font-bold '>{advice}</h2>
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-quote" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 11h-4a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h3a1 1 0 0 1 1 1v6c0 2.667 -1.333 4.333 -4 5" /><path d="M19 11h-4a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h3a1 1 0 0 1 1 1v6c0 2.667 -1.333 4.333 -4 5" /></svg>
-          </div>) :
-          <Skeleton/>
-          } */}
-         
           <div className="flex flex-row items-center justify-end gap-3">
             {/* dark mode */}
-            <ThemeSwitch/>
+            <ThemeSwitch />
 
             {/* avatar and dropdown */}
             <div className="hs-dropdown relative inline-flex [--placement:bottom-right]">
-              <button id="hs-dropdown-with-header" type="button" className="w-[2.375rem] h-[2.375rem] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent  hover:bg-gray-400 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-60 bg-gray-500 text-gray-200">
+              <button
+                id="hs-dropdown-with-header"
+                type="button"
+                className="w-[2.375rem] h-[2.375rem] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent  hover:bg-gray-400 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-60 bg-gray-500 text-gray-200"
+              >
                 <span>G</span>
               </button>
 
               {/* JUST UI FOR NOW */}
-              <div className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-2 dark:bg-gray-800 dark:border dark:border-gray-700" aria-labelledby="hs-dropdown-with-header">
+              <div
+                className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-2 dark:bg-gray-800 dark:border dark:border-gray-700"
+                aria-labelledby="hs-dropdown-with-header"
+              >
                 <div className="py-3 px-5 -m-2 bg-gray-100 rounded-t-lg dark:bg-gray-700">
-                  <p className="text-sm text-gray-500 dark:text-gray-400">Signed in as</p>
-                  <p className="text-sm font-medium text-gray-800 dark:text-gray-300">guest@email.com</p>
+                  <p className="text-sm text-gray-500 dark:text-gray-400">
+                    Signed in as
+                  </p>
+                  <p className="text-sm font-medium text-gray-800 dark:text-gray-300">
+                    guest@email.com
+                  </p>
                 </div>
                 <div className="mt-2 py-2 first:pt-0 last:pb-0">
-                  <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-user" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
+                  <a
+                    className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
+                    href="#"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="icon icon-tabler icon-tabler-user"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                      <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                    </svg>
                     Profile
                   </a>
-                  <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-settings" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" /><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /></svg>
+                  <a
+                    className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
+                    href="#"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="icon icon-tabler icon-tabler-settings"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
+                      <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                    </svg>
                     Settings
                   </a>
-                  <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-logout" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" /><path d="M9 12h12l-3 -3" /><path d="M18 15l3 -3" /></svg>
+                  <a
+                    className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
+                    href="#"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="icon icon-tabler icon-tabler-logout"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+                      <path d="M9 12h12l-3 -3" />
+                      <path d="M18 15l3 -3" />
+                    </svg>
                     Log out
                   </a>
                 </div>
@@ -76,11 +128,11 @@ export default function Header() {
         </div>
       </nav>
     </header>
-  )
+  );
 }
 
 function Skeleton() {
   return (
-    <div className='h-10 bg-opacity-40  py-2 rounded-xl w-1/2  bg-gray-200  dark:bg-gray-700 animate-pulse'></div>
-  )
+    <div className="h-10 bg-opacity-40  py-2 rounded-xl w-1/2  bg-gray-200  dark:bg-gray-700 animate-pulse"></div>
+  );
 }
