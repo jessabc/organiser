55ba13949e34bf9b9b0660bd0494bac7d06345a8
jess <120062852+jessabc@users.noreply.github.com>
2024-04-17 14:57:47 +1000

    fix ts

--- src/app/_helpers/getNumCompletedSubtasks.ts
+++ src/app/_helpers/getNumCompletedSubtasks.ts
@@ -1,6 +1,6 @@
-import { Task } from "../types/interfaces";
+import { ITask } from "../types/interfaces";
 
-export default function getNumCompletedSubtasks(task: Task) {
+export default function getNumCompletedSubtasks(task: ITask) {
   let count = 0;
   task.subtasks.forEach((subtask) => (subtask.isCompleted ? count++ : null));
   return count;
